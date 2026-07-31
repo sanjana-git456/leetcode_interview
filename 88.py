9383c7ff10cf9da2f11fb36cc95ada09c4476799
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-08-01 03:41:15 +0530

    merge 2 sorted arrays

---
+++ 88.py
@@ -0,0 +1,14 @@
+x = list(map(int, input("Enter: ").split()))
+y = list(map(int, input("Enter: ").split()))
+def merge(nums1, nums2):
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
